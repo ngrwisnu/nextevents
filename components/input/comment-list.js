eca6d218dc2efe1990a01d58485882e375538362
ngrwisnu <adiprayogongrwisnu@gmail.com>
2022-11-24 09:32:50 +0700

    connecting comments API to frontend

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,9 +1,18 @@
-import classes from './comment-list.module.css';
+import classes from "./comment-list.module.css";
 
-function CommentList() {
+function CommentList(props) {
+  const { comments } = props;
   return (
     <ul className={classes.comments}>
       {/* Render list of comments - fetched from API */}
+      {comments.map((comment) => (
+        <li key={comment.id}>
+          <p>{comment.comment}</p>
+          <div>
+            By <address>{comment.name}</address>
+          </div>
+        </li>
+      ))}
       <li>
         <p>My comment is amazing!</p>
         <div>
