d2a66faa9360cd99bb99937fa5df6593ce9cf508
ngrwisnu <adiprayogongrwisnu@gmail.com>
2022-11-25 09:08:53 +0700

    add context state

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import Layout from "../components/layout/layout";
 import Notification from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Notification
-        title="Test"
-        message="This is the first test."
-        status="success"
-      />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <Notification
+          title="Test"
+          message="This is the first test."
+          status="success"
+        />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
---
+++ store/notification-context.js
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+const NotificationContext = createContext({
+  notification: null,
+  showNotification: () => {},
+  hideNotification: () => {},
+});
+
+export function NotificationContextProvider(props) {
+  return (
+    <NotificationContext.Provider>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
+
+export default NotificationContext;
